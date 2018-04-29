fcd13268a5af5ebf848558ad335a9376171c74d1
Matthew McCorry <mccorry@gmail.com>
2018-04-29 10:27:48 +0100

    Propper paging solution

--- ShitForum/ViewComponents/PagerViewComponent.cs
+++ ShitForum/ViewComponents/PagerViewComponent.cs
@@ -1,22 +1,15 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Services.Dtos;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace ShitForum.ViewComponents
 {
-    public class PagerViewComponent : ViewComponent
+    public class Pager : ViewComponent
     {
-        public PageData PageData { get; private set; }
-        public List<int> PagesToShow { get; private set; }
-
-        public Task<IViewComponentResult> InvokeAsync(int pageNumber)
+        public IViewComponentResult Invoke(int pageNumber, int numberOfPages)
         {
-            ////this.PageData = pageData;
-            ////var ps = new List<int>() { pageData.PageNumber - 1, pageData.PageNumber, pageData.PageNumber + 1 }.Where(a => a >= 1 && a <= pageData.NumberOfPages);
-            ////this.PagesToShow = ps.ToList();
-            return Task.FromResult<IViewComponentResult>(View());
+            var ps = Enumerable.Range(pageNumber -2, 5).Where(a => a >= 1 && a <= numberOfPages).ToList();
+            return View(new PagerViewModel(pageNumber, ps, numberOfPages));
         }
     }
 }
---
+++ ShitForum/ViewComponents/PagerViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShitForum.ViewComponents
+{
+
+    public class PagerViewModel
+    {
+        public PagerViewModel(int pageNumber, IReadOnlyList<int> pagesToShow, int numberOfPages)
+        {
+            PageNumber = pageNumber;
+            PagesToShow = pagesToShow;
+            NumberOfPages = numberOfPages;
+        }
+
+        public int PageNumber { get; }
+        public IReadOnlyList<int> PagesToShow { get; }
+        public int NumberOfPages { get; }
+    }
+}
