f6ea6188b8ad85b6fe9d03429118f405200f0edd
Matthew McCorry <mccorry@gmail.com>
2018-05-04 23:18:54 +0100

    Added mobile guid view

---
+++ ShitForum/ViewComponents/FriendlyGuid.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace ShitForum.ViewComponents
+{
+
+    public class FriendlyGuid : ViewComponent
+    {
+        public IViewComponentResult Invoke(Guid g) => View(g);
+    }
+}
