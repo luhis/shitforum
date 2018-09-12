238d70cf1cee1b8d54a68198bdc65b4510a65194
Matthew McCorry <mccorry@gmail.com>
2018-09-12 20:29:55 +0100

    added header tests

---
+++ Tests/UnitTests/Headers/ExpectCtShould.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using ShitForum;
+using Xunit;
+
+namespace UnitTests.Headers
+{
+    public class ExpectCtShould
+    {
+        [Fact]
+        public void ExcludeWhenNotHttps()
+        {
+            var mr = new MockRepository(MockBehavior.Strict);
+            var ab = mr.Create<IApplicationBuilder>();
+            var httpRequest = mr.Create<HttpRequest>();
+            httpRequest.Setup(a => a.IsHttps).Returns(false);
+            var httpContext = mr.Create<HttpContext>();
+            httpContext.Setup(s => s.Request).Returns(httpRequest.Object);
+            ab.Setup(a => a.Use(It.IsAny<Func<RequestDelegate, RequestDelegate>>()))
+                .Returns<Func<RequestDelegate, RequestDelegate>>(a =>
+                {
+                    a(_ => Task.CompletedTask)(httpContext.Object);
+                    return ab.Object;
+                });
+            ab.Object.UseExpectCt();
+            mr.VerifyAll();
+        }
+
+        [Fact]
+        public void IncludeWhenHttps()
+        {
+            var mr = new MockRepository(MockBehavior.Strict);
+            var ab = mr.Create<IApplicationBuilder>();
+            var httpRequest = mr.Create<HttpRequest>();
+            httpRequest.Setup(a => a.IsHttps).Returns(true);
+            var httpResponse = mr.Create<HttpResponse>();
+            var hd = new HeaderDictionary();
+            httpResponse.Setup(a => a.Headers).Returns(hd);
+            var httpContext = mr.Create<HttpContext>();
+            httpContext.Setup(s => s.Request).Returns(httpRequest.Object);
+            httpContext.Setup(s => s.Response).Returns(httpResponse.Object);
+            ab.Setup(a => a.Use(It.IsAny<Func<RequestDelegate, RequestDelegate>>()))
+                .Returns<Func<RequestDelegate, RequestDelegate>>(a =>
+                {
+                    a(_ => Task.CompletedTask)(httpContext.Object);
+                    return ab.Object;
+                });
+            ab.Object.UseExpectCt();
+            mr.VerifyAll();
+            hd.Count.Should().Be(1);
+        }
+    }
+}
---
+++ Tests/UnitTests/Headers/UseXContentSecurityPolicyShould.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using ShitForum;
+using Xunit;
+
+namespace UnitTests.Headers
+{
+    public class UseXContentSecurityPolicyShould
+    {
+        [Fact]
+        public void Fail()
+        {
+            var mr = new MockRepository(MockBehavior.Strict);
+            var ab = mr.Create<IApplicationBuilder>();
+            var httpResponse = mr.Create<HttpResponse>();
+            httpResponse.Setup(a => a.Headers).Returns(new HeaderDictionary());
+            var httpContext = mr.Create<HttpContext>();
+            httpContext.Setup(s => s.Response).Returns(httpResponse.Object);
+            ab.Setup(a => a.Use(It.IsAny<Func<RequestDelegate, RequestDelegate>>()))
+                .Returns<Func<RequestDelegate, RequestDelegate>>(a =>
+                {
+                    a(_ => Task.CompletedTask)(httpContext.Object);
+                    return ab.Object;
+                });
+            Action action = () => ab.Object.UseXContentSecurityPolicy();
+            var e = action.Should().Throw<Exception>();
+            e.WithMessage("Content-Security-Policy header must be set before adding IE11 compatibility");
+            mr.VerifyAll();
+        }
+
+        [Fact]
+        public void Success()
+        {
+            var mr = new MockRepository(MockBehavior.Strict);
+            var ab = mr.Create<IApplicationBuilder>();
+            var httpResponse = mr.Create<HttpResponse>();
+            var hd = new HeaderDictionary() { { "Content-Security-Policy", "default-src: self;" } };
+            httpResponse.Setup(a => a.Headers).Returns(hd);
+            var httpContext = mr.Create<HttpContext>();
+            httpContext.Setup(s => s.Response).Returns(httpResponse.Object);
+            ab.Setup(a => a.Use(It.IsAny<Func<RequestDelegate, RequestDelegate>>()))
+                .Returns<Func<RequestDelegate, RequestDelegate>>(a =>
+                {
+                    a(_ => Task.CompletedTask)(httpContext.Object);
+                    return ab.Object;
+                });
+            ab.Object.UseXContentSecurityPolicy();
+            mr.VerifyAll();
+            hd.Count.Should().Be(2);
+        }
+    }
+}
