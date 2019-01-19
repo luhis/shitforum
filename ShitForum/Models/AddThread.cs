b3062bf140a21a5df5b0e8ab59558064674bac34
Matthew McCorry <mccorry@gmail.com>
2019-01-19 09:57:34 +0000

    code cleanup

--- ShitForum/Models/AddThread.cs
+++ ShitForum/Models/AddThread.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using ShitForum.Attributes;
 
 namespace ShitForum.Models
